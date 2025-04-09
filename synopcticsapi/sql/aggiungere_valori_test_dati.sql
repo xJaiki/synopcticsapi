f6802421aedebe2809c144e022a1bd1a83274187
Mario Di Marino <mariodimarinox@gmail.com>
2025-04-09 11:26:49 +0200

    sql

---
+++ synopcticsapi/sql/aggiungere_valori_test_dati.sql
@@ -0,0 +1,7 @@
+﻿-- Inserisci i dati di test per 4 elementi
+INSERT INTO SynopticDatas (ElementId, SynopticLayout, Text1, Text2, Text3, Status, LastUpdate)
+VALUES 
+('Status_12', 'sinoptico_test', 'ABC', 'XYZ', '123', 1, GETDATE()),
+('Status_14', 'sinoptico_test', 'DEF', 'UVW', '456', 2, GETDATE()),
+('Status_40', 'sinoptico_test', 'GHI', 'RST', '789', 0, GETDATE()),
+('Status_42', 'sinoptico_test', 'JKL', 'MNO', '012', 3, GETDATE());
